05880d6e3eb84152af6e0d934a2dfd465bd45536
ATuinDev <1757663+AitorATuin@users.noreply.github.com>
2020-07-14 16:36:30 +0200

    Implement one event loop for each CRD defined using asyncio

--- appgate/__main__.py
+++ appgate/__main__.py
@@ -1,4 +1,22 @@
-from appgate.appgate import event_loop
+import sys
+import asyncio
+
+from appgate.appgate import policies_loop, entitlements_loop, conditions_loop, \
+    init_kubernetes
+
+
+def main() -> None:
+    ns = init_kubernetes()
+    if not ns and len(sys.argv) == 1:
+        print('Unable to discover namespace, please provide it.')
+        sys.exit(1)
+    ns = ns or sys.argv[1]
+    ioloop = asyncio.get_event_loop()
+    ioloop.create_task(policies_loop(ns))
+    ioloop.create_task(entitlements_loop(ns))
+    ioloop.create_task(conditions_loop(ns))
+    ioloop.run_forever()
+
 
 if __name__ == "__main__":
-    event_loop()
+    main()
