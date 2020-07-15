a85506c7f4cad6a5d88cdfab4489a997d1df9138
ATuinDev <1757663+AitorATuin@users.noreply.github.com>
2020-07-15 11:18:21 +0200

    Add fields that were missing in the CRD

--- appgate/types.py
+++ appgate/types.py
@@ -1,5 +1,5 @@
 from attr import attrib, attrs
-from typing import List, Dict, Any
+from typing import List, Dict, Any, Optional
 from typedload import load
 
 __all__ = [
@@ -31,11 +31,19 @@ class Event:
 
 
 @attrs(slots=True)
+class ActionMonitor:
+    enabled: bool = attrib()
+    timeout: int = attrib()
+
+
+@attrs(slots=True)
 class Action:
     subtype: str = attrib()
     action: str = attrib()
     types: str = attrib()
     hosts: List[str] = attrib()
+    ports: List[str] = attrib()
+    monitor: ActionMonitor = attrib()
 
 
 @attrs(slots=True)
@@ -50,17 +58,24 @@ class AppShortcut:
 @attrs(slots=True)
 class Entitlement:
     name: str = attrib()
-    site: str = attrib()
-    conditions: List[str] = attrib()
-    disabled: bool = attrib()
+    notes: str = attrib()
+    create: str = attrib()
+    updated: str = attrib()
     tags: List[str] = attrib()
+    disabled: bool = attrib()
+    site: str = attrib()
+    site_name: str = attrib(metadata={
+        'name': 'siteName'
+    })
     condition_logic: str = attrib(metadata={
         'name': 'conditionLogic'
     })
+    conditions: List[str] = attrib()
     actions: List[Action] = attrib()
     app_shortcut: AppShortcut = attrib(metadata={
         'name': 'appShortcut'
     })
+    app_shortcut_scripts: List[str] = attrib()
 
 
 def entitlement_load(data: Dict[str, Any]) -> Entitlement:
@@ -70,9 +85,31 @@ def entitlement_load(data: Dict[str, Any]) -> Entitlement:
 @attrs(slots=True)
 class Policy:
     name: str = attrib()
+    notes: str = attrib()
+    create: Optional[str] = attrib()
+    updated: Optional[str] = attrib()
     tags: List[str] = attrib()
     disabled: bool = attrib()
     expression: str = attrib()
+    entitlements: List[str] = attrib()
+    entitlement_links: List[str] = attrib(metadata={
+        'name': 'entitlementLinks'
+    })
+    ringfence_rules: List[str] = attrib(metadata={
+        'name': 'ringfenceRules'
+    })
+    ringfence_rule_links: List[str] = attrib(metadata={
+        'name': 'ringfenceRuleLinks'
+    })
+    tamper_proofing: bool = attrib(metadata={
+        'name': 'tamperProofing'
+    })
+    override_site: str = attrib(metadata={
+        'name': 'overrideSite'
+    })
+    administrative_roles: List[str] = attrib(metadata={
+        'name': 'administrativeRoles'
+    })
 
 
 def policy_load(data: Dict[str, Any]) -> Policy:
@@ -80,12 +117,30 @@ def policy_load(data: Dict[str, Any]) -> Policy:
 
 
 @attrs(slots=True)
+class RemedyMethod:
+    type: str = attrib()
+    message: str = attrib()
+    claim_suffix: str = attrib(metadata={
+        'name': 'claimSuffix'
+    })
+    provider_id: str = attrib(metadata={
+        'name': 'providerId'
+    })
+
+
+@attrs(slots=True)
 class Condition:
     name: str = attrib()
+    notes: str = attrib()
+    create: str = attrib()
+    updated: str = attrib()
     tags: List[str] = attrib()
     expression: str = attrib()
     repeat_schedules: List[str] = attrib(metadata={
         'name': 'repeatSchedules'
+    })
+    remedy_methods: List[RemedyMethod] = attrib(metadata={
+        'name': 'remedyMethods'
     })
 
 
