f47e9a95292497bfd3d54cc8b7b145df505da2f3
ATuinDev <1757663+AitorATuin@users.noreply.github.com>
2020-07-15 08:05:42 +0200

    Use logger instead of prints

--- appgate/appgate.py
+++ appgate/appgate.py
@@ -1,5 +1,6 @@
 import asyncio
 import functools
+import logging
 from typing import Optional, Dict, Any, AsyncGenerator
 import kubernetes
 from kubernetes.client import CustomObjectsApi
@@ -17,6 +18,8 @@ __all__ = [
 
 
 crds: Optional[CustomObjectsApi] = None
+log = logging.getLogger('appgate-operator')
+log.setLevel(logging.INFO)
 
 
 def get_crds() -> CustomObjectsApi:
@@ -33,7 +36,8 @@ def init_kubernetes() -> Optional[str]:
 
 async def event_loop(namespace: str, crd: str) -> AsyncGenerator[Dict[str, Any], None]:
     w = kubernetes.watch.Watch()
-    print(f'Loop for {crd}/{namespace} started')
+    log.info(f'[{crd}/{namespace}] Loop for {crd}/{namespace} started')
+    log.debug('test')
     s = w.stream(get_crds().list_namespaced_custom_object, DOMAIN, 'v1',
                  namespace, crd)
     loop = asyncio.get_event_loop()
@@ -47,18 +51,21 @@ async def policies_loop(namespace: str):
     async for event in event_loop(namespace, 'policies'):
         ev = Event(event)
         policy = policy_load(ev.object.spec)
-        print(f'Event type: {ev.type}: {policy}')
+        log.info('[policies/%s}] Event type: %s: %s', namespace,
+                 ev.type, policy)
 
 
 async def entitlements_loop(namespace: str) -> None:
     async for event in event_loop(namespace, 'entitlements'):
         ev = Event(event)
         entitlement = entitlement_load(ev.object.spec)
-        print(f'Event type: {ev.type}: {entitlement}')
+        log.info('[entitlements/%s}] Event type: %s: %s', namespace,
+                 ev.type, entitlement)
 
 
 async def conditions_loop(namespace: str) -> None:
     async for event in event_loop(namespace, 'conditions'):
         ev = Event(event)
         condition = condition_load(ev.object.spec)
-        print(f'Event type: {ev.type}: {condition}')
+        log.info('[conditions/%s}] Event type: %s: %s', namespace,
+                 ev.type, condition)
