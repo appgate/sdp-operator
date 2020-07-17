834f2044d962024e0337700d1cd161e2cc37ae62
ATuinDev <1757663+AitorATuin@users.noreply.github.com>
2020-07-17 15:43:02 +0200

    Fix default values in types

--- appgate/types.py
+++ appgate/types.py
@@ -1,4 +1,4 @@
-from attr import attrib, attrs
+from attr import attrib, attrs, evolve
 from typing import List, Dict, Any, Optional, FrozenSet, TypeVar, Generic, Union
 from typedload import load
 
@@ -78,17 +78,17 @@ class Entitlement(Entity_T):
     site: str = attrib()
     conditions: FrozenSet[str] = attrib(factory=frozenset)
     actions: FrozenSet[Action] = attrib(factory=frozenset)
-    notes: Optional[str] = attrib(default=None)
-    tags: Optional[FrozenSet[str]] = attrib(default=None)
+    notes: Optional[str] = attrib(default='')
+    tags: Optional[FrozenSet[str]] = attrib(factory=frozenset)
     condition_logic: Optional[str] = attrib(metadata={
         'name': 'conditionLogic'
     }, default="and")
     app_shortcut: Optional[AppShortcut] = attrib(metadata={
         'name': 'appShortcut'
     }, default=None)
-    app_shortcut_scripts: Optional[List[str]] = attrib(default=None)
+    app_shortcut_scripts: Optional[FrozenSet[str]] = attrib(factory=frozenset)
     disabled: Optional[bool] = attrib(default=False)
-    id: Optional[str] = attrib(default=None)
+    id: Optional[str] = attrib(default='')
 
 
 def entitlement_load(data: Dict[str, Any]) -> Entitlement:
@@ -99,24 +99,24 @@ def entitlement_load(data: Dict[str, Any]) -> Entitlement:
 class Policy(Entity_T):
     name: str = attrib()
     expression: str = attrib()
-    notes: Optional[str] = attrib(default=None)
+    notes: Optional[str] = attrib(default='')
     override_site: Optional[str] = attrib(metadata={
         'name': 'overrideSite'
-    }, default=None)
+    }, default='')
     tags: Optional[FrozenSet[str]] = attrib(default=None)
-    entitlements: Optional[FrozenSet[str]] = attrib(default=None)
+    entitlements: Optional[FrozenSet[str]] = attrib(factory=frozenset)
     entitlement_links: Optional[FrozenSet[str]] = attrib(metadata={
         'name': 'entitlementLinks'
-    }, default=None)
+    }, factory=frozenset)
     ringfence_rules: Optional[FrozenSet[str]] = attrib(metadata={
         'name': 'ringfenceRules'
-    }, default=None)
+    }, factory=frozenset)
     ringfence_rule_links: Optional[FrozenSet[str]] = attrib(metadata={
         'name': 'ringfenceRuleLinks'
-    }, default=None)
+    }, factory=frozenset)
     administrative_roles: Optional[FrozenSet[str]] = attrib(metadata={
         'name': 'administrativeRoles'
-    }, default=None)
+    }, factory=frozenset)
     disabled: bool = attrib(default=False)
     tamper_proofing: bool = attrib(metadata={
         'name': 'tamperProofing'
@@ -144,14 +144,14 @@ class RemedyMethod:
 class Condition(Entity_T):
     name: str = attrib()
     expression: str = attrib()
-    notes: Optional[str] = attrib(default=None)
-    tags: Optional[FrozenSet[str]] = attrib(default=None)
+    notes: Optional[str] = attrib(default='')
+    tags: Optional[FrozenSet[str]] = attrib(factory=frozenset)
     repeat_schedules: Optional[FrozenSet[str]] = attrib(metadata={
         'name': 'repeatSchedules'
-    }, default=None)
+    }, factory=frozenset)
     remedy_methods: Optional[FrozenSet[RemedyMethod]] = attrib(metadata={
         'name': 'remedyMethods'
-    }, default=None)
+    }, factory=frozenset)
     id: Optional[str] = attrib(default=None)
 
 
