62d7d2882193a8c8e18298ad506376d3951ce69c
ATuinDev <1757663+AitorATuin@users.noreply.github.com>
2020-08-10 15:39:57 +0200

    Add dynamic entities creation
    This PR adds support to parse the openapi specification in yaml and generate
    internally the entities attributes.

---
+++ appgate/attrs.py
@@ -0,0 +1,169 @@
+import uuid
+from pathlib import Path
+from typing import Any, Dict, List, Optional, cast, FrozenSet, Tuple, Callable
+
+from attr import make_class, attrib
+import yaml
+from typedload import load
+
+
+SPEC_DIR = 'api_specs'
+
+
+TYPES_MAP = {
+    'string': str,
+    'boolean': bool,
+    'integer': int,
+}
+
+
+def get_keys(data: Dict[str, Any], keys: List[str]) -> Optional[Any]:
+    """
+    gets key0.key1... entry in `data`
+    """
+    while True:
+        try:
+            k = keys.pop(0)
+        except IndexError:
+            return None
+        # Is the key there?
+        if k in data:
+            # No more entries, return the value
+            if not keys:
+                return data[k]
+            # more entries and we dont have a dict, fail
+            elif type(data[k]) is not dict:
+                return None
+            else:
+                # iterate
+                data = data[k]
+        else:
+            # key not found
+            return None
+
+
+def is_ref(entry: Dict[str, Any]) -> bool:
+    """
+    Checks ig entry is a reference
+    """
+    return '$ref' in entry
+
+
+def is_object(entry: Dict[str, Any]) -> bool:
+    """
+    Checks ig entry is an object
+    """
+    return 'type' in entry and entry['type'] == 'object'
+
+
+def is_array(entry: Dict[str, Any]) -> bool:
+    """
+    Checks ig entry is an array
+    """
+    return 'type' in entry and entry['type'] == 'array'
+
+
+def get_entry(entities: Dict[str, Any], entity: str, entry: List[str],
+              spec_dir: Optional[Path] = None) -> Any:
+    """
+    Resolves entity in entities, if it's not registered it tries to parse it
+    from the specification in disk and saves it in entities.
+    """
+    p = (spec_dir or Path(SPEC_DIR)) / entity
+    v = get_keys(entities, [p.name])
+    if not v:
+        print(f'reading entity from disk: {entity}')
+        with p.open('r') as f:
+            entities[p.name] = yaml.load(f.read())
+    if entry:
+        return get_keys(entities, [p.name] + entry)
+    return v
+
+
+def make_attrib(entity_name: str, attrib_name: str, attrib_props: Dict[str, Any],
+                required_fields: List[str], level: int) -> dict:
+    """
+    Returns an attribs dictionary used later to call attrs.attrib
+    """
+    print(f'Creating attribute {entity_name}.{attrib_name}')
+    required = attrib_name in required_fields
+    tpe, factory = make_type(entity_name, attrib_name, attrib_props, level)
+    attribs = {
+        'type': tpe if required else Optional[tpe],
+    }
+    if level == 0 and attrib_name == 'id':
+        attribs['factory'] = lambda: str(uuid.uuid4())
+    elif not required:
+        attribs['default'] = None
+    elif factory:
+        attribs['factory'] = factory
+
+    return attribs
+
+
+def attrs_has_default(attrs) -> bool:
+    """
+    Checks if attrs as a default field value
+    """
+    return 'default' in attrs or 'factory' in attrs
+
+
+def make_attribs(entity_name: str, attributes, level: int) -> Dict[str, int]:
+    """
+    Returns the attr.attrib data needed to use attr.make_class
+    """
+    entity_attrs = {}
+    entity_attrs_attrib = {}
+    for s in attributes:
+        required_fields = s.get('required', [])
+        properties = s['properties']
+        for attrib_name, attrib_props in properties.items():
+            entity_attrs[attrib_name] = make_attrib(entity_name, attrib_name, attrib_props, required_fields, level)
+
+    # We need to create then in order. Those with default values at the end
+    for attrib_name, attrib_attrs in {k: v for k,v in entity_attrs.items()
+                                      if not attrs_has_default(v)}.items():
+        entity_attrs_attrib[attrib_name] = attrib(**attrib_attrs)
+    for attrib_name, attrib_attrs in {k: v for k, v in entity_attrs.items()
+                                      if attrs_has_default(v)}.items():
+        entity_attrs_attrib[attrib_name] = attrib(**attrib_attrs)
+    return entity_attrs_attrib
+
+
+def make_type(entity_name: str, attrib_name: str, data,
+              level: int) -> Tuple[type, Optional[Callable[[], Any]]]:
+    tpe = data['type']
+    if tpe in TYPES_MAP:
+        return TYPES_MAP[tpe], None
+    elif is_array(data):
+        # Recursion here, we parse the items as a type
+        array_tpe, _ = make_type(attrib_name, attrib_name, data['items'], level)
+        return FrozenSet[array_tpe], frozenset  # type: ignore
+    elif is_object(data):
+        # Indirect recursion here.
+        name = f'{entity_name}_{attrib_name.capitalize()}'
+        return make_class(name, make_attribs(entity_name, [data], level + 1)), None
+    else:
+        raise Exception(f'Unknown type: {tpe}')
+
+
+def make_entity(entity: str, spec_dir: Optional[Path] = None) -> type:
+    """
+    Function that creates an entity class from a yaml specification
+    """
+    entity_yml = f'{entity}.yml'
+    entity_name = entity.capitalize()
+    scheme = get_entry(entities={}, entity=entity_yml, spec_dir=spec_dir,
+                       entry=['definitions', entity_name, 'allOf'])
+    data: dict = {}
+    attributes = []
+    for s in scheme:
+        if is_ref(s):
+            p, k = s['$ref'].split('#', maxsplit=2)
+            print(f'resolving reference: {p} {k}')
+            resolved_ref = get_entry(entities=data, entity=p, spec_dir=spec_dir,
+                                     entry=[x for x in k.split('/') if x])
+            attributes.append(resolved_ref)
+        elif is_object(s):
+            attributes.append(s)
+    return make_class(entity_name, make_attribs(entity_name, attributes, 0))
