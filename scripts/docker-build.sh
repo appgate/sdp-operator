facb3ac79c979a5d9859c9f7dc50fd6a48fd6d2b
ATuinDev <1757663+AitorATuin@users.noreply.github.com>
2020-09-03 13:18:41 +0200

    gcc is needed when building

--- scripts/docker-build.sh
+++ scripts/docker-build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 apt-get update
-apt-get install libffi-dev make --yes
+apt-get install libffi-dev make gcc --yes
 # cffi requires libffi 7 but python-slim only has 6 :/
 ln -s /usr/lib/x86_64-linux-gnu/libffi.so.6 /usr/lib/x86_64-linux-gnu/libffi.so.7
 if [ "$1" == "shell" ]; then
