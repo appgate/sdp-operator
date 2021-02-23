b65040e59110cc59450f94d8a456b48cf69ec2f1
ATuinDev <1757663+AitorATuin@users.noreply.github.com>
2021-02-23 11:46:36 +0100

    Improve syntax
     - Don't use double negations (looks weird)
     - Use default value when getting ENV variable

--- appgate/appgate.py
+++ appgate/appgate.py
@@ -112,12 +112,13 @@ def get_context(args: OperatorArguments,
     dry_run_mode = os.getenv(DRY_RUN_ENV) or ('1' if args.dry_run else '0')
     cleanup_mode = os.getenv(CLEANUP_ENV) or ('1' if args.cleanup else '0')
     spec_directory = os.getenv(SPEC_DIR_ENV) or args.spec_directory or SPEC_DIR
-    no_verify = (os.getenv(APPGATE_SSL_NO_VERIFY) or '0') == '1' or args.no_verify
+    no_verify = (os.getenv(APPGATE_SSL_NO_VERIFY), '0') == '1' or args.no_verify
     appgate_cacert = os.getenv(APPGATE_SSL_CACERT)
     appgate_cacert_path = None
-    if not no_verify and appgate_cacert:
+    verify = not no_verify
+    if verify and appgate_cacert:
         appgate_cacert_path = save_cert(appgate_cacert)
-    elif not no_verify and args.cafile:
+    elif verify and args.cafile:
         appgate_cacert_path = args.cafile
     secrets_key = os.getenv(APPGATE_SECRETS_KEY)
     target_tags_arg = frozenset(args.target_tags) if args.target_tags else frozenset()
