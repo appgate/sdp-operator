5cd9d75cbef145cc924fac2a8d88fe800764864d
Thomas Cellerier <thomas.cellerier@appgate.com>
2022-01-04 16:26:19 +0100

    Fix unit tests

--- tests/test_entities.py
+++ tests/test_entities.py
@@ -568,7 +568,21 @@ def test_certificate_pem_load():
         },
     }
     e0 = APPGATE_LOADER.load(e0_data, None, EntityCert)
-    assert e0.fieldOne is None
+    assert e0.fieldOne == """\
+-----BEGIN CERTIFICATE-----
+MIICEjCCAXsCAg36MA0GCSqGSIb3DQEBBQUAMIGbMQswCQYDVQQGEwJKUDEOMAwG
+A1UECBMFVG9reW8xEDAOBgNVBAcTB0NodW8ta3UxETAPBgNVBAoTCEZyYW5rNERE
+MRgwFgYDVQQLEw9XZWJDZXJ0IFN1cHBvcnQxGDAWBgNVBAMTD0ZyYW5rNEREIFdl
+YiBDQTEjMCEGCSqGSIb3DQEJARYUc3VwcG9ydEBmcmFuazRkZC5jb20wHhcNMTIw
+ODIyMDUyNjU0WhcNMTcwODIxMDUyNjU0WjBKMQswCQYDVQQGEwJKUDEOMAwGA1UE
+CAwFVG9reW8xETAPBgNVBAoMCEZyYW5rNEREMRgwFgYDVQQDDA93d3cuZXhhbXBs
+ZS5jb20wXDANBgkqhkiG9w0BAQEFAANLADBIAkEAm/xmkHmEQrurE/0re/jeFRLl
+8ZPjBop7uLHhnia7lQG/5zDtZIUC3RVpqDSwBuw/NTweGyuP+o8AG98HxqxTBwID
+AQABMA0GCSqGSIb3DQEBBQUAA4GBABS2TLuBeTPmcaTaUW/LCB2NYOy8GMdzR1mx
+8iBIu2H6/E2tiY3RIevV2OW61qY2/XRQg7YPxx3ffeUugX9F4J/iPnnu1zAxxyBy
+2VguKv4SWjRFoRkIfIlHX0qVviMhSlNy2ioFLy7JcPZb+v3ftDGywUqcBiVDoea0
+Hn+GmxZA
+-----END CERTIFICATE-----"""
     assert e0.fieldTwo == cert
 
     e1_data = {
@@ -578,7 +592,7 @@ def test_certificate_pem_load():
     e1 = K8S_LOADER.load(e1_data, None, EntityCert)
     assert e1.fieldOne == PEM_TEST
     assert e1.fieldTwo == cert
-    assert e1 == EntityCert(fieldOne="Crap data that is ignored", fieldTwo=cert)
+    assert e1 == EntityCert(fieldOne=e0.fieldOne, fieldTwo=cert)
     assert e1 == e0
 
     cert2 = EntityCert_Fieldtwo(
