d259f0616b311ddb8ced047387aefbb8f0d5e9e5
Aitor <aitor.iturri@appgate.com>
2022-01-05 13:29:00 +0100

    Fix directory to unzip the specs

--- bin/unzip-open-spec.sh
+++ bin/unzip-open-spec.sh
@@ -1,15 +1,12 @@
 #!/usr/bin/env bash
 
 set -ex
-
+mkdir api_specs
 VERSIONS="12 13 14 15 16"
 for v in 12 13 14 15 16; do
-    unzip /tmp/openspec-$v.zip -d api_spec
-    mv api_spec/sdp-api-specification-version-$v api_spec/v$v
-    rm -rf api_spec/v$v/.github
-    rm api_spec/v$v/README.md
+    unzip /tmp/openspec-$v.zip -d api_specs
+    mv api_specs/sdp-api-specification-version-$v api_specs/v$v
+    rm -rf api_specs/v$v/.github
+    rm api_specs/v$v/README.md
     rm /tmp/openspec-$v.zip
 done
-
-echo "TEST"
-ls -la
