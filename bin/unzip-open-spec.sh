3185cad26d6dcfc5f58b2a76feba458d0089ec04
Aitor <aitor.iturri@appgate.com>
2022-01-05 13:13:31 +0100

    OFC it's a directory :D

--- bin/unzip-open-spec.sh
+++ bin/unzip-open-spec.sh
@@ -6,7 +6,7 @@ VERSIONS="12 13 14 15 16"
 for v in 12 13 14 15 16; do
     unzip /tmp/openspec-$v.zip -d api_spec
     mv api_spec/sdp-api-specification-version-$v api_spec/v$v
-    rm api_spec/v$v/.github
+    rm -rf api_spec/v$v/.github
     rm api_spec/v$v/README.md
     rm /tmp/openspec-$v.zip
 done
