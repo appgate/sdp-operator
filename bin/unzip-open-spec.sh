7d8c0eae13f36935657e63a33488dc75244853f9
Aitor <aitor.iturri@appgate.com>
2022-01-05 13:01:06 +0100

    Upload artifacts in zip format

---
+++ bin/unzip-open-spec.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+set -ex
+
+VERSIONS="12 13 14 15 16"
+for v in 12 13 14 15 16; do
+    unzip /tmp/openspec-$v.zip -d api_spec
+    mv api_spec/sdp-api-specification-version-$v api_spec/$v
+    rm /tmp/openspec-$v.zip
+done
