31df4bcb957f26896f2dfed81d07e0b3d6d9ba14
Masaya Ando <masaya.ando@appgate.com>
2022-05-31 10:52:05 -0400

    Expand CRD test

--- tests/test_crd.py
+++ tests/test_crd.py
@@ -1,11 +1,36 @@
 from pathlib import Path
 
+import yaml
+
 from appgate.openapi.openapi import generate_crd, generate_api_spec, SPEC_DIR
 
 
-def test_generate_crd():
-    for version in ["v12", "v13", "v14", "v15", "v16"]:
-        open_api = generate_api_spec(Path(SPEC_DIR).parent / version)
-        for k, v in open_api.entities.items():
-            crd = generate_crd(open_api.entities[k].cls, {})
-            assert crd
+ENTITIES_TO_TEST = {"Appliance", "Policy", "Entitlement", "Condition", "IdentityProvider"}
+
+
+def assert_equal_crd(version: str) -> None:
+    open_api = generate_api_spec(Path(SPEC_DIR).parent / version)
+    for entity in ENTITIES_TO_TEST:
+        crd = generate_crd(open_api.entities[entity].cls, {})
+        with (Path("tests/resources/crd") / version / entity.lower()).with_suffix(".yaml").open("r") as f:
+            assert yaml.safe_load(crd) == yaml.safe_load(f)
+
+
+def test_generate_crd_v12():
+    assert_equal_crd("v12")
+
+
+def test_generate_crd_v13():
+    assert_equal_crd("v13")
+
+
+def test_generate_crd_v14():
+    assert_equal_crd("v14")
+
+
+def test_generate_crd_v15():
+    assert_equal_crd("v15")
+
+
+def test_generate_crd_v16():
+    assert_equal_crd("v16")
