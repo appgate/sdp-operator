a49cd324a77a131c871f8f385d4a5b17cb16f2a1
Masaya Ando <masaya.ando@appgate.com>
2022-06-15 11:53:26 -0400

    Use v17 API for unit testing

--- appgate/openapi/openapi.py
+++ appgate/openapi/openapi.py
@@ -33,7 +33,7 @@ __all__ = [
 ]
 
 
-SPEC_DIR = "api_specs/v12"
+SPEC_DIR = "api_specs/v17"
 K8S_API_VERSION = "apiextensions.k8s.io/v1"
 K8S_CRD_KIND = "CustomResourceDefinition"
 LIST_PROPERTIES = {"range", "data", "query", "orderBy", "descending", "filterBy"}
