12dd3f13c079385356d14f201fbd3b564ae9f5b6
Masaya Ando <masaya.ando@appgate.com>
2022-06-29 10:53:06 -0400

    Update CRDs for v14

--- tests/test_crd.py
+++ tests/test_crd.py
@@ -19,6 +19,7 @@ def assert_equal_crd(version: str) -> None:
     open_api = generate_api_spec(Path(SPEC_DIR).parent / version)
     for entity in ENTITIES_TO_TEST:
         crd = generate_crd(open_api.entities[entity].cls, {}, version)
+
         with (Path("tests/resources/crd") / version / entity.lower()).with_suffix(
             ".yaml"
         ).open("r") as f:
