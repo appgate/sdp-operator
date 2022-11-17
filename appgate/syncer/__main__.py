187f99f117739ba110cd7e6a483314d51115f1b5
Masaya Ando <masaya.ando@appgate.com>
2022-11-17 10:20:37 -0500

    Create entity in each directory, load more env, print configuration

--- appgate/syncer/__main__.py
+++ appgate/syncer/__main__.py
@@ -1,12 +1,8 @@
 import asyncio
 from asyncio import Queue
-import datetime
 import os
 from pathlib import Path
-import shutil
 import sys
-import time
-import logging
 import yaml
 from typing import List
 
@@ -18,7 +14,7 @@ from appgate.types import (
     AppgateEventError,
     EntityWrapper,
 )
-from appgate.__main__ import init_kubernetes, get_context
+from appgate.__main__ import init_kubernetes, APPGATE_LOG_LEVEL
 from appgate.client import K8SConfigMapClient
 from appgate.appgate import start_entity_loop
 from appgate.openapi.openapi import entity_names
@@ -62,7 +58,9 @@ def dump(ctx: Context, entity: Entity_T):
     dumped_entities: List[str] = []
     entity_type = entity.__class__.__qualname__.lower()
 
-    file = DUMP_DIR / f"{entity_type}.yaml"
+    entity_dir = DUMP_DIR / f"{entity_type}-{ctx.api_spec.api_version}"
+    entity_dir.mkdir(exist_ok=True)
+    entity_file = entity_dir / f"{entity.name.lower().replace(' ', '-')}.yaml"
     dumped_entity = dump_entity(
         EntityWrapper(entity), entity_type, str(ctx.api_spec.api_version)
     )
@@ -70,23 +68,39 @@ def dump(ctx: Context, entity: Entity_T):
     appgate_metadata = dumped_entity["spec"].get(APPGATE_METADATA_ATTRIB_NAME)
     if appgate_metadata:
         entity_passwords = appgate_metadata.get(APPGATE_METADATA_PASSWORD_FIELDS_FIELD)
-    dumped_entities.append(yaml.safe_dump(dumped_entity, default_flow_style=False))
-    f = file.open("w") if file else sys.stdout
-    for i, de in enumerate(dumped_entities):
-        if i > 0:
-            f.write("---\n")
-        f.write(de)
-    if file:
-        f.close()
+    dumped_entities.append(
+        yaml.safe_dump(dumped_entity, default_flow_style=False, sort_keys=True)
+    )
+    with entity_file.open("w") as f:
+        for i, de in enumerate(dumped_entities):
+            if i > 0:
+                f.write("---\n")
+            f.write(de)
+
+
+def print_configuration(ctx: Context):
+    log.info("[git-syncer] Starting Git Syncer loop with the following configuration: ")
+    log.info(
+        "[git-syncer]     Target tags: %s",
+        ",".join(ctx.target_tags) if ctx.target_tags else "None",
+    )
+    log.info("[git-syncer]     Log level: %s", os.getenv(APPGATE_LOG_LEVEL, "info"))
+    log.info("[git-syncer]     Timeout: %s", ctx.timeout)
+    log.info("[git-syncer]     Git repository: %s", os.getenv("GIT_REPOSITORY_URL", ""))
+    log.info("[git-syncer]     Git vendor: %s", os.getenv("GIT_VENDOR", ""))
+    log.info("[git-syncer]     Git username: %s", os.getenv("GIT_USERNAME", ""))
+    log.info("[git-syncer]     Git base branch: %s", os.getenv("GIT_BASE_BRANCH", ""))
 
 
 async def main_loop(queue: Queue, ctx: Context) -> None:
     entities: List[Entity_T] = []
     error_events: List[AppgateEventError] = []
 
-    git: GitRepo = get_git_repository("github")
+    git: GitRepo = get_git_repository()
     git.check_env_vars()
     git.clone_repository(DUMP_DIR)
+
+    print_configuration(ctx)
 
     while True:
         try:
@@ -99,8 +113,7 @@ async def main_loop(queue: Queue, ctx: Context) -> None:
                 continue
 
             log.info(
-                "[appgate-operator/%s}] Event: %s %s with name %s",
-                ctx.namespace,
+                '[git-syncer] Event: %s entity of type %s "%s"',
                 event.op,
                 event.entity.__class__.__qualname__,
                 event.entity.name,
@@ -108,12 +121,10 @@ async def main_loop(queue: Queue, ctx: Context) -> None:
             entities.append(event.entity)
 
         except asyncio.exceptions.TimeoutError:
-            log.info(f"Timeout {ctx.timeout} seconds expired")
             if error_events:
                 for event_error in error_events:
                     log.error(
-                        "[appgate-operator/%s}] - Entity of type %s with name %s : %s",
-                        ctx.namespace,
+                        "[git-syncer] Entity of type %s %s: %s",
                         event_error.name,
                         event_error.kind,
                         event_error.error,
@@ -126,28 +137,20 @@ async def main_loop(queue: Queue, ctx: Context) -> None:
                 dump(ctx, entity)
 
             if git.needs_pull_request():
-                log.info("Detected changes in the git repository.")
+                log.info("[git-syncer] Found changes in the git repository")
                 git.commit_change()
                 git.push_change()
                 git.create_pull_request()
+            else:
+                log.info(
+                    f"[git-syncer] No changes in the git repository. Sleeping {ctx.timeout} seconds"
+                )
 
 
 if __name__ == "__main__":
     try:
-        set_level(log_level="info")
-        asyncio.run(
-            run_git_syncer(
-                OperatorArguments(
-                    namespace="sdp-system",
-                    spec_directory="api_specs/v17",
-                    host="https://envy-10-97-180-2.devops:8443",
-                    user="admin",
-                    password="admin",
-                    no_verify=True,
-                    timeout="180",
-                )
-            )
-        )
+        set_level(log_level=os.getenv(APPGATE_LOG_LEVEL, "info"))
+        asyncio.run(run_git_syncer(OperatorArguments()))
     except KeyboardInterrupt:
         log.info("Interrupted by user.")
         sys.exit(1)
