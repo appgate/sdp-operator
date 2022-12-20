df42f68dd535f2bf834c4ea02caa67cdf2979b88
Aitor Iturri <aitor.iturri@appgate.com>
2022-12-20 08:47:05 +0100

    Create k8s entity clients

--- appgate/appgate.py
+++ appgate/appgate.py
@@ -441,7 +441,7 @@ async def operator(
                 )
                 async with AsyncExitStack() as exit_stack:
                     appgate_client = None
-                    k8s_client = None
+                    k8s_api = None
                     if not ctx.dry_run_mode and not ctx.reverse_mode:
                         if ctx.device_id is None:
                             raise AppgateException("No device id specified")
@@ -458,7 +458,7 @@ async def operator(
                             )
                         )
                     elif not ctx.dry_run_mode and ctx.reverse_mode:
-                        k8s_client = K8sEntityClient()
+                        k8s_api = CustomObjectsApi()
                     else:
                         log.warning(
                             "[%s/%s] Running in dry-mode, nothing will be created",
@@ -474,7 +474,10 @@ async def operator(
                         )
                         if appgate_client
                         else {},
-                        k8s_entity_clients={"FIXME": k8s_client} if k8s_client else {},
+                        k8s_entity_clients=generate_k8s_clients(
+                            api_spec=ctx.api_spec, namespace=ctx.namespace, k8s_api=k8s_api,
+                        )
+                        if k8s_api else {},
                         k8s_configmap_client=k8s_configmap_client,
                         api_spec=ctx.api_spec,
                     )
@@ -496,7 +499,7 @@ async def operator(
                         expected_appgate_state = (
                             expected_appgate_state.sync_generations()
                         )
-                    elif k8s_client:
+                    elif k8s_api:
                         current_appgate_state = new_plan.appgate_state
                         expected_appgate_state = await get_current_appgate_state(
                             ctx=ctx
