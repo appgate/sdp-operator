39331c4c1ecafabe8b4b626b887eb66fcc397d71
Aitor Iturri <aitor.iturri@appgate.com>
2023-01-10 14:57:39 +0100

    Use GitHub SSH fingerprints

--- appgate/syncer/git.py
+++ appgate/syncer/git.py
@@ -35,6 +35,14 @@ from appgate.types import (
 )
 
 
+# GitHub SSH fingerprints
+# see: https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/githubs-ssh-key-fingerprints
+GITHUB_SSH_FINGERPRINT = """github.com ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIOMqqnkVzrm0SdG6UOoqKLsabgH5C9okWi0dh2l9GKJl
+github.com ecdsa-sha2-nistp256 AAAAE2VjZHNhLXNoYTItbmlzdHAyNTYAAAAIbmlzdHAyNTYAAABBBEmKSENjQEezOmxkZMy7opKgwFB9nkt5YRrYMjNuG5N87uRgg6CLrbo5wAdT/y6v0mKV0U2w0WZ2YB/++Tpockg=
+github.com ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEAq2A7hRGmdnm9tUDbO9IDSwBK6TbQa+PXYPCPy6rbTrTtw7PHkccKrpp0yVhp5HdEIcKr6pLlVDBfOLX9QUsyCOV0wzfjIJNlGEYsdlLJizHhbn2mUjvSAHQqZETYP81eFzLQNnPHt4EVVUh7VfDESU84KezmD5QlWpXLmvU31/yMf+Se8xhHTvKSCZIFImWwoG6mbUoWf9nzpIoaSjB+weqqUUmpaaasXVal72J+UX2B+2RPW3RcT0eOzQgqlJL3RKrTJvdsjE3JEAvGq3lGHSZXy28G3skua2SmVi/w4yCE6gbODqnTWlg7+wC604ydGXA8VJiS5ap43JXiUFFAaQ==
+"""
+
+
 class EnvironmentVariableNotFoundException(Exception):
     pass
 
@@ -324,6 +332,15 @@ class GitHubRepo(GitRepo):
 def get_git_repository(ctx: GitOperatorContext) -> GitRepo:
     if ctx.git_vendor.lower() == "github":
         log.info("[git-operator] Detected GitHub as git vendor type")
+        known_hosts = Path.home() / ".ssh/known_hosts"
+        known_hosts.parent.mkdir(exist_ok=True)
+        # Add GITHUB ssh fingerprints into known_hosts
+        # TODO: Add support to override this?
+        log.info(
+            "[git-operator] Creating file with known SSH fingerprints for github.com"
+        )
+        with known_hosts.open("w") as f:
+            print(GITHUB_SSH_FINGERPRINT, file=f)
         return github_repo(ctx, GIT_DUMP_DIR)
     else:
         raise Exception(f"Unknown git vendor type {ctx.git_vendor}")
