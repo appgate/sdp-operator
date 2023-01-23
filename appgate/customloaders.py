f6cb08697292fcc186344cf6ae0b5e68c829003d
Masaya Ando <masaya.ando@appgate.com>
2023-01-23 15:34:49 -0500

    Return empty string on file load if field doesn't exist

--- appgate/customloaders.py
+++ appgate/customloaders.py
@@ -50,12 +50,16 @@ class FileAttribLoader(CustomAttribLoader):
 
     def load(self, values: AttributesDict) -> AttributesDict:
         v = values.get(self.field)
-        if not v and self.load_external:
-            # Try loading the s value from the external source if value doesn't exist
-            v = self.loader(values)
-            if not v:
-                return values
-            values[self.field] = v
+        if not v:
+            if self.load_external:
+                # Try loading the s value from the external source if value doesn't exist
+                v = self.loader(values)
+                if not v:
+                    return values
+                values[self.field] = v
+            else:
+                # Return empty string, otherwise the loader will complain if we return None
+                values[self.field] = ""
         return values
 
 
