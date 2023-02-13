86e9241711844f1dfae117654ccc6fac2183f996
Aitor Iturri <aitor.iturri@appgate.com>
2023-02-13 10:56:25 +0100

    Add some more unit tests for appgatefiles

--- tests/test_appgatefiles.py
+++ tests/test_appgatefiles.py
@@ -58,6 +58,51 @@ def test_load_http_file_1():
         assert expected_entity == e
 
 
+@patch.dict(os.environ, {"APPGATE_FILE_SOURCE": "http"})
+@patch.dict(os.environ, {"APPGATE_FILE_HTTP_ADDRESS": "localhost:8000"})
+@patch.dict(os.environ, {"APPGATE_API_VERSION": "v18"})
+def test_load_http_file_2():
+    EntityTestFileComplex = (
+        load_test_open_api_spec(secrets_key=None, reload=True)
+        .entities["EntityTestFileComplex"]
+        .cls
+    )
+    # NOTE that now file is present
+    data = {"filename": "test-entity.sh", "file": "SE9MQUNBUkFDT0xB"}
+    expected_entity = EntityTestFileComplex(
+        filename="test-entity.sh",
+        file="c3RhcnQxMjM=",
+        checksum="2c4779e28ec964baa2afdeb862be4b9776562866443cfcf22f37950c20ed0af2",
+    )
+
+    with patch("appgate.files.requests.get") as get:
+        mock_response = Response()
+        mock_response._content = b"start123"
+        mock_response.status_code = 200
+        get.return_value = mock_response
+        e = K8S_LOADER.load(data, None, EntityTestFileComplex)
+        get.assert_called_once_with(
+            "localhost:8000/entitytestfilecomplex-v18/test-entity.sh"
+        )
+        assert expected_entity == e
+
+
+def test_load_http_file_3():
+    EntityTestFileComplex = (
+        load_test_open_api_spec(secrets_key=None, reload=True)
+        .entities["EntityTestFileComplex"]
+        .cls
+    )
+    data = {"filename": "test-entity.sh"}
+    expected_entity = EntityTestFileComplex(
+        filename="test-entity.sh",
+        file="c3RhcnQxMjM=",
+        checksum="2c4779e28ec964baa2afdeb862be4b9776562866443cfcf22f37950c20ed0af2",
+    )
+    e = K8S_LOADER.load(data, None, EntityTestFileComplex)
+    assert expected_entity == e
+
+
 @patch.dict(os.environ, {"APPGATE_FILE_SOURCE": "s3"})
 @patch.dict(os.environ, {"APPGATE_API_VERSION": "v18"})
 def test_load_s3_file_0():
@@ -107,3 +152,46 @@ def test_load_s3_file_1():
             "sdp", "entitytestfilecomplex-v18/test-entity.sh"
         )
         assert expected_entity == e
+
+
+@patch.dict(os.environ, {"APPGATE_FILE_SOURCE": "s3"})
+@patch.dict(os.environ, {"APPGATE_API_VERSION": "v18"})
+def test_load_s3_file_2():
+    EntityTestFileComplex = (
+        load_test_open_api_spec(secrets_key=None, reload=True)
+        .entities["EntityTestFileComplex"]
+        .cls
+    )
+    data = {"filename": "test-entity.sh", "file": "SE9MQUNBUkFDT0xB"}
+    expected_entity = EntityTestFileComplex(
+        filename="test-entity.sh",
+        file="c3RhcnQxMjM=",
+        checksum="2c4779e28ec964baa2afdeb862be4b9776562866443cfcf22f37950c20ed0af2",
+    )
+
+    with patch("appgate.files.Minio.bucket_exists") as bucket_exists, patch(
+        "appgate.files.Minio.get_object"
+    ) as get_object:
+        bucket_exists.return_value = True
+        get_object.return_value = urllib3.response.HTTPResponse(body=b"start123")
+        e = K8S_LOADER.load(data, None, EntityTestFileComplex)
+        get_object.assert_called_once_with(
+            "sdp", "entitytestfilecomplex-v18/test-entity.sh"
+        )
+        assert expected_entity == e
+
+
+def test_load_s3_file_3():
+    EntityTestFileComplex = (
+        load_test_open_api_spec(secrets_key=None, reload=True)
+        .entities["EntityTestFileComplex"]
+        .cls
+    )
+    data = {"filename": "test-entity.sh",}
+    expected_entity = EntityTestFileComplex(
+        filename="test-entity.sh",
+        file="c3RhcnQxMjM=",
+        checksum="2c4779e28ec964baa2afdeb862be4b9776562866443cfcf22f37950c20ed0af2",
+    )
+    e = K8S_LOADER.load(data, None, EntityTestFileComplex)
+    assert expected_entity == e
