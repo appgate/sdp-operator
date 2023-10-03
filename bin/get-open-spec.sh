245683e4c8a7f9f83dafbc845991aa8d3c80b288
Masaya Ando <masaya.ando@appgate.com>
2023-10-03 18:01:12 -0400

    Revert "Only download API v16 - v19"
    This reverts commit 741858d4903f39e2ca91a077f81a5a84460a66f5.

--- bin/get-open-spec.sh
+++ bin/get-open-spec.sh
@@ -2,7 +2,7 @@
 
 set -ex
 
-for v in $(seq 16 19); do
+for v in $(seq 12 19); do
     rm -f /tmp/openspecs-$v.zip
     wget https://github.com/appgate/sdp-api-specification/archive/refs/heads/version-$v.zip \
          -O /tmp/openspec-$v.zip
